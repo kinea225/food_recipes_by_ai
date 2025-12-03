74cfde31cda6775e83f4104e4edff54e7d5cc14d
kinea225 <kinea225@naver.com>
2025-12-04 00:09:40 +0900

    build commit2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 아래 줄을 추가하세요. '/리포지토리이름/' 형식이어야 합니다.
-  base: "./", 
+  base: "/food_recipes_by_ai/", 
 })
